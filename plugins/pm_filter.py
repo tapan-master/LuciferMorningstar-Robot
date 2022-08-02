af0bfd155a96554b146718b73bfbe44a84c43587
TG Dragon <110241656+TG-Dragon@users.noreply.github.com>
2022-08-03 03:40:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -372,9 +372,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         settings = await get_settings(query.message.chat.id)
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
+                f_caption = CUSTOM_FILE_CAPTION.format((mention=query.from_user.mention, title=title, size=size, caption=files.caption)
             except Exception as e:
                 logger.exception(e)
             f_caption = f_caption
@@ -425,10 +423,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         f_caption = files.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
-            except Exception as e:
+                f_caption = CUSTOM_FILE_CAPTION.format((mention=query.from_user.mention, title=title, size=size, caption=files.caption)
                 logger.exception(e)
                 f_caption = f_caption
         if f_caption is None:
