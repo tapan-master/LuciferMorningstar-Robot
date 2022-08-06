982cc74aaf14cc283c972012295d397c26f26da7
TG Dragon <110241656+TG-Dragon@users.noreply.github.com>
2022-08-06 18:40:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ git clone -b master https://github.com/TG-Dragon/LuciferMorningstar-Robot.git /L
 cd /LuciferMorningstar-Robot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting LuciferMorningstar-Robot..."
-python3 bot.py
+echo "Starting LuciferMorningstar Robot..."
+python 3 bot.py
