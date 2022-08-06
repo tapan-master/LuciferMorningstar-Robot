30db1e86f477295c97f3d70d800eadf17fb7a552
TG Dragon <110241656+TG-Dragon@users.noreply.github.com>
2022-08-06 22:52:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/TG-Dragon/LuciferMorningstar-Robot.git /LuciferMorningstar-Robot
+git clone -b master https://github.com/tapan-master/LuciferMorningstar-Robot.git /LuciferMorningstar-Robot
 cd /LuciferMorningstar-Robot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
